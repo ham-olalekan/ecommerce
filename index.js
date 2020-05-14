26ab66d7ddfa8ec69795136e28a80cd898777ca7
Hammed Lukman Olalekan <hammedlukman@gmail.com>
2020-05-14 18:31:09 +0100

    add initial project bootstrap

---
+++ index.js
@@ -0,0 +1,17 @@
+const winston = require("winston");
+const express = require("express");
+const config = require("config");
+const app = express();
+
+
+require("./startup/cors")(app);
+require("./startup/routes")(app);
+require("./startup/db")();
+require("./startup/config")();
+
+const port = process.env.PORT || config.get("port");
+const server = app.listen(port, () =>
+  winston.info(`Listening on port ${port}...`)
+);
+
+module.exports = server;
