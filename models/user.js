50b482201d39dc79c6256ad2f5e1f3235bdb8166
Hammed Lukman Olalekan <hammedlukman@gmail.com>
2020-05-15 00:18:06 +0100

    add endpoint to create user

--- models/user.js
+++ models/user.js
@@ -1,50 +1,67 @@
 const Joi = require("joi");
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
-    minlength: 2,
-    maxlength: 50,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minlength: 2,
+      maxlength: 50,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      minlength: 2,
+      maxlength: 50,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+      maxlength: 11,
+      minlength: 11,
+      unique: true,
+    },
+    gender: {
+      type: String,
+      required: false,
+    },
+    address: {
+      type: String,
+      required: false,
+    },
+    dateRegistered: {
+      type: Date,
+      default: Date.now,
+    },
+    email: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 255
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 1024,
+    },
+    dateRegistered: {
+        type: Date,
+        default: Date.now
+    }
   },
-  lastName: {
-    type: String,
-    required: true,
-    minlength: 2,
-    maxlength: 50,
-  },
-  phoneNumber: {
-    type: String,
-    required: true,
-    maxlength: 11,
-    minlength: 11,
-    unique: true,
-  },
-  gender: {
-    type: String,
-    required: false,
-  },
-  address: {
-    type: String,
-    required: false,
-  },
-  dateRegistered: {
-    type: Date,
-    default: Date.now,
-  },
-  email: {
-    type: String,
-    required: true,
-    minlength: 5,
-    maxlength: 255,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 5,
-    maxlength: 1024,
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.set("toJSON", {
+  transform: function (doc, returnedResult, options) {
+    returnedResult.id = returnedResult._id;
+    delete returnedResult._id;
+    delete returnedResult.__v;
+    delete returnedResult.updatedAt;
   },
 });
 
@@ -57,6 +74,8 @@ function validateUserObject(user) {
     phoneNumber: Joi.string().min(11).max(15).required(),
     email: Joi.string().min(5).max(255).required().email(),
     password: Joi.string().min(5).max(255).required(),
+    address: Joi.string().min(5).max(255),
+    gender: Joi.string().min(4).max(6)
   };
 
   return Joi.validate(user, schema);
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,27 @@
 const express = require("express");
 const router = express.Router();
+const _ = require("lodash");
+const { createUser } = require("../services/userService");
+/**
+ * endpoint to create a new user
+ */
+router.post("/", async (req, res) => {
+  const user = req.body;
 
+  try {
+    const dataObj = await createUser(user);
+    res
+      .status(201)
+      .send({ status: true, data: _.pick(dataObj.data, [ "id", "firstName", "lastName", "phoneNumber", "address", "email", "dateRegistered"])});
+  } catch (error) {
+      console.log(`Error occured while trying to create user `, error);
+    res
+    .status(error.statusCode || 400)
+    .send({
+      status: false,
+      message: error.message,
+    });
+  }
+});
 
-
-
-
-module.exports =router;
+module.exports = router;
